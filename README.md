5a803adfa225fd145aed68d07102b652c49090c3
atyrode <alex.tyrode@outlook.fr>
2023-03-10 19:31:36 +0100

    better tmux.sh with lot of options

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ Python and requirements.txt
 A Crossbar router
 Minecraft_template
 A PostgreSQL server
+
+Tmux.sh options:
+
+`--install` to install the requirements.txt and eventually update the packages
+`--prune` to prune the docker containers (useful with the next option)
+`--docker` which runs the docker compose
+and `--run` to automatically run the base components of the template
+
+With no options, and by default, the tmux.sh only create a tmux session and split it evenly with 9 tabs
--- tmux.sh
+++ tmux.sh
@@ -1,28 +1,46 @@
 #!/bin/bash
-if [[ $1 == "--install" ]]; then
+if [[ "$@" == *"--install"* ]]; then
     echo "Installing requirements..."
     sudo apt-get install libpq-dev
     pip install -r requirements.txt --upgrade
 
 fi
 
+if [[ "$@" == *"--prune"* ]]; then
+    echo "Pruning docker images..."
+    sudo docker system prune -a
+fi
+
+if [[ "$@" == *"--docker"* ]]; then
+  export POSTGRES_USER=$(grep -o '"server_ip": *"[^"]*"' config.json | sed 's/"//g' | awk -F': *' '{print $2}')
+  export POSTGRES_PASSWORD=$(grep -o '"rcon_password": *"[^"]*"' config.json | sed 's/"//g' | awk -F': *' '{print $2}')
+
+  mkdir -p ./volumes/database
+  mkdir -p ./volumes/world
+  sudo docker compose up -d
+fi
+
 # Save current working directory
 cwd=$(pwd)
 
-# Fill .tmux.conf file with desired settings
-echo "Setting tmux options..."
-
 cd ~
-git clone https://github.com/gpakosz/.tmux.git
-ln -s -f .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local .
-sed -i '/#set -g mouse on/s|^#||' .tmux.conf.local
 
-echo "Going back to the original directory..."
+if [ ! -d ".tmux" ]; then
+  # Fill .tmux.conf file with desired settings
+  echo "Preparing tmux options..."
+  git clone https://github.com/gpakosz/.tmux.git
+  ln -s -f .tmux/.tmux.conf
+  cp .tmux/.tmux.conf.local .
+  sed -i '/#set -g mouse on/s|^#||' .tmux.conf.local
+fi
+
 cd $cwd
 
-echo "------------------------------"
-
+if tmux has-session -t stream >/dev/null 2>&1; then
+    echo "Attaching to existing session..."
+    tmux attach-session -t stream
+    exit
+fi
 
 # Create the tmux session
 tmux new-session -d -s stream
@@ -45,7 +63,7 @@ tmux select-layout tiled
 
 # Run the python scripts
 # parse the args to see if there's "-run"
-if [[ $1 == "--run" ]]; then
+if [[ "$@" == *"--run"* ]]; then
     echo "Running the scripts..."
     tmux send-keys -t 1 'python3 components/collector.py' C-m
     tmux send-keys -t 2 'python3 components/poster.py' C-m
@@ -57,3 +75,8 @@ if [[ $1 == "--run" ]]; then
     tmux send-keys -t 8 'python3 components/comment.py' C-m
     tmux send-keys -t 9 'python3 components/share.py' C-m
 fi
+
+echo "test"
+tmux attach-session -t stream
+
+echo "Done!"
