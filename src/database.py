ff647bef45bbe81efa5f944cd6075513204c3c49
atyrode <alex.tyrode@outlook.fr>
2023-09-16 07:14:23 +0200

    relaunch if out of asyncio loop

--- src/database.py
+++ src/database.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 import shulker as mc
 from dill import dumps
 from tools.postgres import db
@@ -46,5 +47,12 @@ class Database(Portal):
 
 if __name__ == "__main__":
     action = Database()
-    asyncio.run(action.run())
+    while True:
+        try:
+            asyncio.run(action.run(), debug=True)
+        except Exception as e:
+            error_msg = f"An error occurred: {e}"
+            print(error_msg)
+            print("-> Restarting in 1 seconds...")
+            time.sleep(1)
 
