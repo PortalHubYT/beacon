0b5094301ef1606e3b80826950335c5af9acb3d2
atyrode <alex.tyrode@outlook.fr>
2023-09-16 07:15:35 +0200

    format and escape backslash

--- src/tools/sanitize.py
+++ src/tools/sanitize.py
@@ -1,38 +1,41 @@
 import string
 from .config import config
 
+
 def pick_display(nickname, unique_id):
-  
-  if nickname:
-    name = sanitize(nickname)
-    if len(name) < len(nickname) / 4:
-      name = "@" + unique_id
-      
-  elif unique_id:
-    name = sanitize(unique_id)
-    name = "@" + name
-    
-  else:
-    return None
-  
-  name = crop(name)
-  return name
-  
+    if nickname:
+        name = sanitize(nickname)
+        if len(name) < len(nickname) / 4:
+            name = "@" + unique_id
+
+    elif unique_id:
+        name = sanitize(unique_id)
+        name = "@" + name
+
+    else:
+        return None
+
+    name = crop(name)
+    return name.replace("\\", "\\\\")
+
+
 def sanitize(text):
     """Remove non-ASCII characters from string s"""
-    return ''.join(filter(lambda x: x in string.printable, text))
-  
+    return "".join(filter(lambda x: x in string.printable, text))
+
+
 def crop(text, max=config.crop_size):
     # Shorten text longer than (config.crop_size) characters and end with '...'
     text = text[:max] + "..." if len(text) > max else text
     return text
 
+
 # =================================================================================================
 
-def role_parser(event):
 
+def role_parser(event):
     role = None
-    
+
     if event.user.is_moderator:
         role = "Moderator"
     elif event.user.is_top_gifter:
@@ -41,7 +44,7 @@ def role_parser(event):
         role = "New Gifter"
     elif event.user.is_following:
         role = "Follower"
-                    
+
     return role
 
 
@@ -57,7 +60,9 @@ def get_profile(event):
         "following": event.user.info.following,
         "comment": None if "comment" not in vars(event) else event.comment,
         "gift": None if "gift" not in vars(event) else event.gift.info.name,
-        "gift_value": None if "gift" not in vars(event) else event.gift.info.diamond_count
+        "gift_value": None
+        if "gift" not in vars(event)
+        else event.gift.info.diamond_count,
     }
-    
+
     return profile
