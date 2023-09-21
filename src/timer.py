3fbe996f77641538615eb90f4a474650b8f3f565
atyrode <alex.tyrode@outlook.fr>
2023-09-21 06:54:48 +0200

    timer working

--- src/timer.py
+++ src/timer.py
@@ -15,34 +15,61 @@ class Timer(Portal):
 
         await self.subscribe("gl.reload_config", self.reload_config)
         await self.subscribe("timer.set", self.set_timer)
+        await self.subscribe("timer.reset", self.reset_timer)
         await self.subscribe("timer.build", self.build_timer)
         await self.subscribe("timer.remove", self.remove_timer)
         await self.subscribe("timer.reload", self.reload_timer)
         await self.initialize_timer()
 
-    def draw_progress_bar(value, config):
-        origin = config["timer_start"].offset(
-            x=config.timer_border_thickness, y=config.timer_border_thickness
-        )
+    async def draw_progress_bar(self, value, draw=False):
+        def draw_lambda(origin, lines):
+            for x in range(len(lines[0])):
+                for y in range(len(lines)):
+                    pos = origin.offset(x=x, y=y, z=-1)
+                    mc.set_block(pos, lines[y][x])
 
-        palette = config["timer_palette"]
+        origin = self.config.timer_start.offset(
+            x=self.config.timer_border_thickness, y=self.config.timer_border_thickness
+        )
+        palette = self.config.timer_palette
         width = (
-            config.width
-            + config.backboard_extra_size * 2
-            + config.timer_border_thickness * 2
+            self.config.width
+            + self.config.backboard_extra_size * 2
+            + self.config.timer_border_thickness
         )
-        height = config.timer_height
-        lines = [[] for _ in range(height + 1)]
-        for i in range(height):
-            while len(lines[i]) < width:
-                block = config.timer_palette[(len(lines[i]) + i) % len(palette)]
-                lines[i].append(block)
+        height = self.config.timer_height + 1  # whi + 1 ? Idk
 
-        for y, line in enumerate(lines):
-            for x, block in enumerate(line):
-                pos = origin.offset(x=x, y=y, z=-1)
-                # print(block)
-                mc.set_block(pos, block)
+        if draw:
+            lines = [[] for _ in range(height)]
+            for y in range(height):
+                while len(lines[y]) <= width:
+                    block = self.config.timer_palette[
+                        (len(lines[y]) + y) % len(palette)
+                    ]
+                    lines[y].append(block)
+
+            if value < 100:
+                bar_x = int(width * (value / 100))
+                for y in range(height):
+                    lines[y][bar_x] = "black_concrete"
+                white_x = bar_x + 1
+                for x in range(width - white_x):
+                    for y in range(height):
+                        lines[y][x + white_x] = "snow_block"
+
+            f = lambda: draw_lambda(origin, lines)
+            await self.publish("mc.lambda", dumps(f))
+        else:
+            pos1 = origin.offset(x=int(width * (value / 100)), z=-1)
+            pos2 = origin.offset(x=width, y=height - 1, z=-1)
+            zone = mc.BlockZone(pos1, pos2)
+
+            f = lambda: mc.set_zone(zone, "snow_block")
+            await self.publish("mc.lambda", dumps(f))
+
+            zone = mc.BlockZone(pos1, pos1.offset(y=height - 1))
+            f = lambda: mc.set_zone(zone, "black_concrete")
+            await self.publish("mc.lambda", dumps(f))
 
     async def reload_timer(self):
         await self.remove_timer()
@@ -164,12 +191,7 @@ class Timer(Portal):
                 f = lambda: mc.set_zone(f_zone, "black_concrete")
                 await self.publish("mc.lambda", dumps(f))
 
-        # print(self.draw_progress_bar(50, self.config))
-        config_copy = copy.deepcopy(self.config)
-        # args = (50, config_copy)
-
-        f = lambda: self.draw_progress_bar(50, config_copy)
-        await self.publish("mc.lambda", dumps(f))
+        await self.draw_progress_bar(100, draw=True)
 
     async def reload_config(self):
         importlib.reload(tools.config)
@@ -178,8 +200,13 @@ class Timer(Portal):
     async def initialize_timer(self):
         print("-> Initializing timer")
 
+    async def reset_timer(self):
+        print("-> Reset timer")
+        await self.draw_progress_bar(100, draw=True)
+
     async def set_timer(self, value):
         print("-> Setting timer to", value)
+        await self.draw_progress_bar(value)
 
 
 if __name__ == "__main__":
