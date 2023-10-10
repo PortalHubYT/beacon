2f0d4145cc1b9a417e4ede5325dc1bfc3a2674e6
atyrode <alex.tyrode@outlook.fr>
2023-10-10 05:46:01 +0200

    moved cmds

--- src/guesses.py
+++ src/guesses.py
@@ -14,6 +14,14 @@ class Guesses(Portal):
         self.colors = ["light_purple", "aqua", "dark_green", "green", "yellow", "red", "dark_red", "dark_purple"]
         self.color_index = 0
         self.coords = mc.Coordinates(-6, 69, 23)
+
+        cmds = [
+            """setblock 7 70 22 minecraft:repeating_command_block[conditional=false,facing=up]{Command:"/kill @e[type=minecraft:text_display, distance=0..2]",CustomName:'{"text":"@"}',LastExecution:1992554L,SuccessCount:0,TrackOutput:0b,UpdateLastExecution:1b,auto:1b,conditionMet:1b,powered:1b}""",
+            """setblock 8 70 22 minecraft:repeating_command_block[conditional=false,facing=south]{Command:"/kill @e[type=minecraft:chicken, distance=0..2]",CustomName:'{"text":"@"}',LastExecution:1986171L,SuccessCount:0,TrackOutput:0b,UpdateLastExecution:1b,auto:1b,conditionMet:1b,powered:1b}""",
+            """setblock 9 70 22 minecraft:repeating_command_block[conditional=false,facing=south]{Command:"execute as @e[type=chicken] at @s run tp ~0.1 ~ ~",CustomName:'{"text":"@"}',LastExecution:1992910L,SuccessCount:140,TrackOutput:0b,UpdateLastExecution:1b,auto:1b,conditionMet:1b,powered:1b}""",
+            ]
+        for cmd in cmds: 
+            await self.publish("mc.post", cmd)
         await self.loop()
         
     async def on_comment(self, user):
@@ -35,10 +43,7 @@ class Guesses(Portal):
             next_user = self.chat_queue.pop(0)
             name = next_user["display"]
             comment = next_user["comment"]
-            #those commands in command block make chicken move and die 
-            #/kill @e[type=minecraft:text_display, distance=0..2]
-            # /kill @e[type=minecraft:chicken, distance=0..2]
-            # execute as @e[type=chicken] at @s run tp ~0.1 ~ ~
+            
             cmd = """summon chicken $coords {NoGravity:1b,NoAI:1b,Tags:["guess"],Passengers:[{see_through:0b,id:"minecraft:text_display",text:'[{"text":"$pseudo","color":"white","bold":true},{"text":"\n$comment","color":"$color","bold":true}]',background:$background}],ActiveEffects:[{Id:14,Amplifier:1b,Duration:4000,ShowParticles:0b}]}"""
             cmd = cmd.replace("$pseudo", f"{name}:")
             cmd = cmd.replace("$comment", f"{comment}")
