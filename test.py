facf7ae68ca45c21d4cb09441813dee6079c99ef
atyrode <alex.tyrode@outlook.fr>
2023-10-19 01:31:36 +0200

    Delete test.py
    didnt mean to push that

--- test.py
+++
@@ -1,9 +0,0 @@
-import time
-
-# This file will repeadetly write to test.log incrementing numbers
-i = 0
-while True:
-    i += 1
-    with open("test.log", "a") as f:
-        f.write(f"{i}\n")
-    time.sleep(1)
