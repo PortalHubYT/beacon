de8e0f4fd504c3fe39e756e6054d6e681372c1fd
atyrode <alex.tyrode@outlook.fr>
2023-10-24 06:13:31 +0200

    update tmux layout

--- start_tmux.sh
+++ start_tmux.sh
@@ -1,43 +1,56 @@
+#!/bin/bash
+
+# Ensure a numeric argument is provided
+if ! [[ "$1" =~ ^[0-9]+$ ]]; then
+    echo "Please provide a numeric argument."
+    exit 1
+fi
+
 # Create a new tmux session named "stream"
 tmux new-session -d -s stream
 
-# Split the window into multiple panes
-tmux split-window -v
-tmux split-window -v
-tmux select-pane -t 1
-tmux split-window -h
-tmux select-pane -t 3
-tmux split-window -h
-tmux split-window -h
+# Determine the dimensions of the grid
+panes=$1
+sqrt=$(echo "sqrt($1)" | bc)
+closest_square=$(( sqrt * sqrt ))
 
-# Get the width of the screen
-screen_width=$(tmux display-message -p '#{window_width}')
+if [[ $closest_square -eq $panes ]]; then
+    rows=$sqrt
+    cols=$sqrt
+else
+    rows=$(( sqrt + 1 ))
+    cols=$sqrt
+    remainder=$(( panes - closest_square ))
+fi
 
-# Calculate the desired width for the pane
-pane_width=$((screen_width * 33 / 100))
+# Create the grid of panes
+created_panes=1
+for (( r=0; r<rows; r++ )); do
+    for (( c=0; c<cols; c++ )); do
+        if [[ $created_panes -eq $panes ]]; then
+            break 2
+        fi
+        tmux split-window -h
+        tmux select-layout tiled
+        created_panes=$(( created_panes + 1 ))
+    done
+    if [[ $created_panes -eq $panes ]]; then
+        break
+    fi
+    tmux split-window -v
+    tmux select-layout tiled
+    created_panes=$(( created_panes + 1 ))
+done
 
-# Resize the panes
-tmux select-pane -t 3
-tmux resize-pane -x "$pane_width"
-tmux resize-pane -y 25
-tmux select-pane -t 4
-tmux resize-pane -x "$pane_width"
-tmux select-pane -t 5
-tmux resize-pane -x "$pane_width"
+# Activate the "venv_skrib" Python virtual environment in each pane
+for pane in $(tmux list-panes -t stream -F '#P'); do
+    tmux select-pane -t $pane
+    tmux send-keys "conda deactivate" C-m
+    tmux send-keys "source ./venv_skrib/bin/activate" C-m
+    tmux send-keys "cd src" C-m
+    tmux send-keys "clear" C-m
 
-# Go to each pane and activate the "venv_skrib" Python virtual environment
-tmux select-pane -t 0
-tmux send-keys "source ./venv_skrib/bin/activate" C-m
-tmux select-pane -t 1
-tmux send-keys "source ./venv_skrib/bin/activate" C-m
-tmux select-pane -t 2
-tmux send-keys "source ./venv_skrib/bin/activate" C-m
-tmux select-pane -t 3
-tmux send-keys "source ./venv_skrib/bin/activate" C-m
-tmux select-pane -t 4
-tmux send-keys "source ./venv_skrib/bin/activate" C-m
-tmux select-pane -t 5
-tmux send-keys "source ./venv_skrib/bin/activate" C-m
+done
 
 # Attach to the tmux session
 tmux attach-session -t stream
